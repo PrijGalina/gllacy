458f9ede183ada982c9466b9fe539944b1a1f09c
PrijGalina <g_prij@mail.ru>
2021-03-18 15:12:07 +0300

    Изменила название переменных, закончила стили для главной

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
+
+function initMap() {
+  let coordinatesmarker = {lat: 59.938635, lng: 30.323118};
+  let coordinates = {lat: 59.938897, lng: 30.328693};
+  let image = "img/pin.svg";
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: coordinates,
+      zoom: 15,
+      disableDefaultUI: true,
+      scrollwheel: false
+  });
+  marker = new google.maps.Marker({
+    position: coordinatesmarker,
+    map: map,
+    icon: image
+  });
+}
+
+function addScript(src){
+  var script = document.createElement('script');
+  script.src = src;
+  script.async = false; 
+  document.head.appendChild(script);
+}
+addScript("https://maps.googleapis.com/maps/api/js?callback=initMap&key=AIzaSyAImiABCDquzpZQ4aQHQU-g89xQEHinWHM"); 
+
 (function () {
   var func = EventTarget.prototype.addEventListener;
-
+  
   EventTarget.prototype.addEventListener = function (type, fn, capture) {
       this.func = func;
       capture = capture || {};
